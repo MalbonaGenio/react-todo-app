9d2be077be54af1cb6b307a8b41e7dbb6c1824dd
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-03-04 08:50:08 +0900

    Map components Now the task are generated trough a map that checks the todosData and insert the data in each todo

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import todosData from "./todosData";
 
 function App() {
-  return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  );
+  const todoComponent = todosData.map((data) => (
+    <TodoItem key={data.id} data={data} />
+  ));
+  return <div className="todo-list">{todoComponent}</div>;
 }
 
 export default App;
