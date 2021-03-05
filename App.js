54ca7245340bd93e21a6e8e55a91bc66a78c4bd1
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-03-05 10:29:46 +0900

    converted from function to a class componnet

--- App.js
+++ App.js
@@ -2,11 +2,20 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
-function App() {
-  const todoComponent = todosData.map((data) => (
-    <TodoItem key={data.id} data={data} />
-  ));
-  return <div className="todo-list">{todoComponent}</div>;
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData,
+    }
+  }
+  render() {
+    const todoComponent = this.state.todos.map((data) => (
+      <TodoItem key={data.id} data={data} />
+    ));
+    return <div className="todo-list">{todoComponent}</div>;
+  }
+  
 }
 
 export default App;
