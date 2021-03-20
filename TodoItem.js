13197abdf8f0dffb01b05fdc995420524b0426a7
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-03-21 08:29:17 +0900

    completed item styles Using conditional rendering apply some styles to the completed todos

--- TodoItem.js
+++ TodoItem.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 function TodoItem(props) {
+  // styles to apply when completed todo
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through"
+  };
   return (
     <div className="todo-item">
       <input
@@ -9,7 +15,9 @@ function TodoItem(props) {
         // onChange recives the event object in the function and then calls handleChange where we pass the id
         onChange={() => props.handleChange(props.data.id)}
       />
-      <p>{props.data.text}</p>
+      <p style={props.data.completed ? completedStyle : null}>
+        {props.data.text}
+      </p>
     </div>
   );
 }
